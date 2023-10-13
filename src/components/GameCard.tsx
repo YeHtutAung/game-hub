0129ec6df94808b2acf0f56e9fdaa9762ddcbbd9
Ye Htut Aung <yehtutaung.yha@gmail.com>
2023-10-13 21:24:18 +0800

    Building game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Game } from "../hook/useGame";
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 
-const GameCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface Props {
+  game: Game
 }
 
-export default GameCard
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card borderRadius={10} overflow='hidden'>
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize='2xl'>{game.name}</Heading>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
